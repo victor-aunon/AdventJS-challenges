25bb42552d47f5b73309a08265921ee185478e38
victor-aunon <hola@victoraunon.com>
2022-10-06 22:52:03 +0200

    Comment challenge 4

--- src/04.js
+++ src/04.js
@@ -5,10 +5,14 @@ export function createXmasTree(height) {
   const trunk = "#";
   let tree = "";
   for (let floor = 0; floor < height; floor++) {
+    // Left half
     tree += `${space.repeat(height - floor - 1)}${leaf.repeat(floor)}`;
+    // Tree center
     tree += `${leaf}`;
+    // Right half
     tree += `${leaf.repeat(floor)}${space.repeat(height - floor - 1)}\n`;
   }
+  // Trunk lines
   tree += `${space.repeat(height - 1)}${trunk}${space.repeat(height - 1)}\n`;
   tree += `${space.repeat(height - 1)}${trunk}${space.repeat(height - 1)}`;
   return tree;
